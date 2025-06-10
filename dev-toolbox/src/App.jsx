5e0df0abf894696e0f7882bcd3619e44d2aca922
pranav <pranavkp170@gmail.com>
2025-06-10 10:32:53 +0530

    Initialize react and tailwind setup

---
+++ dev-toolbox/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className='container bg-red-500 mx-auto'>
+        <h1>Hello tailwindcssv4 and react</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
